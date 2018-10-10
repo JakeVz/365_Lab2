373f092a63213b8bd22e8e361d283222f35fcb04
jakevz <jakevz122@gmail.com>
2018-10-10 13:45:37 -0700

    added marathon

---
+++ MARATHON/MARATHON-setup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE marathon (
+   Place INTEGER PRIMARY KEY,
+   Time TIME,
+   Pace TIME,
+   GroupPlace INTEGER, 
+   GroupLabel CHAR(10),
+   Age INTEGER,
+   Sex CHAR(1),
+   BIBNumber INTEGER,
+   FirstName CHAR(30),
+   LasName CHAR(30),
+   Town CHAR(30),
+   State CHAR(2)
+);
